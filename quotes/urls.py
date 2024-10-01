f6de683623b887989c5b8906957dca43534c0d02
ralzaid <ralzaid@bu.edu>
2024-09-30 20:00:31 -0400

    trying to fix css issues

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-# from django.conf import settings
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('quote/', views.random_quote, name='quote'),
     path('show_all/', views.show_all, name='show_all'),
     path('about/', views.about, name='about'),
-]
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
