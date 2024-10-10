d7ba97d866d56c88f5ae5ef19313f0c953c53ede
ralzaid <ralzaid@bu.edu>
2024-10-09 20:08:28 -0400

    add txt file

--- cs412/urls.py
+++ cs412/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quotes/', include('quotes.urls')),
     path('restaurant/', include('restaurant.urls')),
     path('mini_fb/', include('mini_fb.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
